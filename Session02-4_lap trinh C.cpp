c78b3641b3e75872adb58202929cc5bfbe98971e
B24DTCN268-hoangvanquan <hoangvanquan1154@gmail.com>
2024-11-21 06:33:39 +0700

    Bài tập session02-4

---
+++ Session02-4_lap trinh C.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  
+    int side = 3; 
+    int perimeter, area;    
+    perimeter = 4 * side;   
+    area = side * side;      
+    printf("Canh hinh vuông là: %d\n", side);
+    printf("Chu vi hinh vuông là: %d\n", perimeter);
+    printf("Dien tích hinh vuông là: %d\n", area);
+
+    return 0;
+}
