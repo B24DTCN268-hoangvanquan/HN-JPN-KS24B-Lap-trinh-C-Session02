ae74d291479f63b0b83c5d47bf3a91cc2f83bc7f
B24DTCN268-hoangvanquan <hoangvanquan1154@gmail.com>
2024-11-21 05:59:17 +0700

    Bài tập session02-1

---
+++ Session02-1_lap trinh C.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int a; //Dung de luu tru so nguyen
+	float b; //Dung de luu tru so thuc
+	char c; //Dung de luu tru ky tu
+	double d; //Dung de luu tru so thuc chinh xac hon float 
+	return 0;
+}
